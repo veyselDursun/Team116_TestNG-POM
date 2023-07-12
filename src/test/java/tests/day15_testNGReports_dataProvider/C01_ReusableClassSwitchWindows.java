bdbf8ae5ea8c0a9c4878df02749235cfd8a76979
veyselDursun <veysdursun@yahoo.com>
2023-07-12 10:00:32 +0200

    day15

---
+++ src/test/java/tests/day15_testNGReports_dataProvider/C01_ReusableClassSwitchWindows.java
@@ -0,0 +1,23 @@
+package tests.day15_testNGReports_dataProvider;
+
+import org.testng.annotations.Test;
+import utilities.Driver;
+
+public class C01_ReusableClassSwitchWindows {
+
+    @Test
+    public void switchWindowTesti(){
+
+    //https://the-internet.herokuapp.com/windows adresine gidin.
+        Driver.getDriver().get("https://the-internet.herokuapp.com/windows");
+
+    //Click Here butonuna basın.
+
+
+    //Acilan yeni pencerenin sayfa başlığının (title) "New Window" oldugunu dogrulayin.
+
+
+    // sayfalari kapatin
+
+    }
+}
